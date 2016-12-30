71d119d87142e89b92db6851169dc05133d3ab13
wilsonwys <wilsonwys@163.com>
2016-12-30 15:12:05 +0800

    webgl切多窗口闪屏bug

---
+++ work/demo/X5FlashDemo/src/settings.js
@@ -0,0 +1,34 @@
+_CCSettings = {
+    "platform": "web-mobile",
+    "groupList": [
+        "default"
+    ],
+    "collisionMatrix": [
+        [
+            true
+        ]
+    ],
+    "rawAssets": {
+        "assets": {
+            "6aa0aa6a-ebee-4155-a088-a687a6aadec4": [
+                "HelloWorld.png",
+                "cc.Texture2D"
+            ]
+        }
+    },
+    "launchScene": "db://assets/helloworld.fire",
+    "scenes": [
+        {
+            "url": "db://assets/helloworld.fire",
+            "uuid": "2d2f792f-a40c-49bb-a189-ed176a246e49"
+        }
+    ],
+    "packedAssets": {
+        "05a04532b": [
+            "2d2f792f-a40c-49bb-a189-ed176a246e49",
+            "31bc895a-c003-4566-a9f3-2e54ae1c17dc"
+        ]
+    },
+    "orientation": "portrait",
+    "debug": true
+};
